9ab8a6865170f1bab8b5bbeadbae2a8b51089408
Char Ryder <charlotte.emilyryder@gmail.com>
2021-08-17 17:47:42 +0100

    Editted about page, restructured files.

--- content/js/app.js
+++ content/js/app.js
@@ -10,25 +10,25 @@ document.addEventListener("DOMContentLoaded",(e)=>{
 
 //countdown - does not work. switching from document.byid to document.queryselector
 
-const days = document.querySelector("#days .count .countdownNumb");
-const hours = document.querySelector("#hours .countdownNumb");
-const minutes = document.querySelector("#minutes .countdownNumb");
-const seconds = document.querySelector("#seconds .countdownNumb");
+//const days = document.querySelector("#days .count .countdownNumb");
+//const hours = document.querySelector("#hours .countdownNumb");
+//const minutes = document.querySelector("#minutes .countdownNumb");
+//const seconds = document.querySelector("#seconds .countdownNumb");
 
-var timer = setInterval(()=>{
-    var launchDate = new Date("2021-09-1").getTime();
-    var now = new Date().getTime();
-    var duration = launchDate - now;
+//var timer = setInterval(()=>{
+    //var launchDate = new Date("2021-09-1").getTime();
+    //var now = new Date().getTime();
+   // var duration = launchDate - now;
 
-    var d = Math.floor(duration / (1000 * 60 * 60 * 24));
-    var h = Math.floor((duration % (100 * 60 * 60 * 24)) / (100 * 60 * 60));
-    var m = Math.floor((duration % (100 * 60 * 60)) / (100 * 60));
-    var s = Math.floor((duration % (1000 * 60)) / 1000);
+   // var d = Math.floor(duration / (1000 * 60 * 60 * 24));
+   // var h = Math.floor((duration % (100 * 60 * 60 * 24)) / (100 * 60 * 60));
+   // var m = Math.floor((duration % (100 * 60 * 60)) / (100 * 60));
+  //  var s = Math.floor((duration % (1000 * 60)) / 1000);
 
-    days.innerHTML = d;
-    hours.innerHTML = h;
-    minutes.innerHTML = m;
-    seconds.innerHTML = s;
+   // days.innerHTML = d;
+  //  hours.innerHTML = h;
+  //  minutes.innerHTML = m;
+  //  seconds.innerHTML = s;
 
-},1000);
+//},1000);
 
