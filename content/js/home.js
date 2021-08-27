4df4f0b89a6f568ea48972df333feeac88cd03da
Char Ryder <charlotte.emilyryder@gmail.com>
2021-08-27 12:19:12 +0100

    Switched from CSS to SCSS, re-arranged JS to avoid errors.

--- content/js/home.js
+++ content/js/home.js
@@ -1,3 +1,36 @@
+//slideshow
+let slideIndex = 1;
+const slides = document.querySelectorAll("article.slide");
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function plusSlides(n){
+    showSlides(slideIndex +=n);
+
+}
+
+function showSlides(n){
+
+    if (n > slides.length){
+        slideIndex=1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++){
+        slides[i].style.display= "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+
+}
+
+showSlides(slideIndex);
+
 //splash
 
 const splash = document.querySelector(".splash");
